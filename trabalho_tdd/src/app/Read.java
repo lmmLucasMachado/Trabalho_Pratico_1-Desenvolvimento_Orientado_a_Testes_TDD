9ef4e598e3dd1820a2a8f26c47fac356d4e8691f
lmmLucasMachado <lmlucasmachadom@gmail.com>
2020-11-25 16:56:30 -0300

    Implemantation read data

--- trabalho_tdd/src/app/Read.java
+++ trabalho_tdd/src/app/Read.java
@@ -6,10 +6,12 @@ public class Read{
 
     static String arq = "";
 
-    public static String read_data(){
-        /*
+    public static String read_data(String name){
+        name = "doc/"+name+".out";
+        //System.out.println(name);
+        
         try{
-            BufferedReader buffer = new BufferedReader(new FileReader("../doc/analysisTime.out"));
+            BufferedReader buffer = new BufferedReader(new FileReader(name));
 
             while(buffer.ready()){
                 String linha = buffer.readLine();
@@ -18,10 +20,11 @@ public class Read{
             buffer.close();
             System.out.println(arq);
         }catch(IOException ioe){
-            ioe.printStackTrace("ArquivoNaoEncontradoException");
+        	System.out.println("ArquivoNaoEncontradoException");
+            ioe.printStackTrace();
         }
-        */
-        arq = "---------- Evolution 1 ----------\n439\n705\n738\n729";
+        
+        //arq = "---------- Evolution 1 ----------\n439\n705\n738\n729";
 
         //System.out.println(arq.length());
 
